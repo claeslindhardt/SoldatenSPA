885d630465979ee7dd7b563cbacbb412853d99c2
claeslindhardt1 <claeslindhardt@gmail.com>
2019-10-13 17:10:19 +0200

    Made a nav bar

---
+++ src/main/webapp/precache-manifest.0204df7f5e2c2b6e62c697263d4c5e2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815d36efd0ec069b1afed4b1ef85d513",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0409bd3ef4afe1d85885",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1626c9c77438101f8785",
+    "url": "/static/css/main.1b1bf1a0.chunk.css"
+  },
+  {
+    "revision": "0409bd3ef4afe1d85885",
+    "url": "/static/js/2.273cfb32.chunk.js"
+  },
+  {
+    "revision": "1626c9c77438101f8785",
+    "url": "/static/js/main.7eaf26d8.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  },
+  {
+    "revision": "3b4e1b16da8e8f133a63225b3131eb8b",
+    "url": "/static/media/logo.3b4e1b16.png"
+  }
+]);
---
+++ src/main/webapp/precache-manifest.9a5f8b1e7033a7a44e43c0c5385507ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525ea3eb2e6aee784192507effd357c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbe3de085f0248028c21",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "24596e6f65b352a01af9",
+    "url": "/static/css/main.a4a666fc.chunk.css"
+  },
+  {
+    "revision": "cbe3de085f0248028c21",
+    "url": "/static/js/2.803bdd0b.chunk.js"
+  },
+  {
+    "revision": "24596e6f65b352a01af9",
+    "url": "/static/js/main.d7830eee.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  }
+]);
