f96ef0b77945f60bdc99de6c60ccc35f10a811a2
claeslindhardt1 <claeslindhardt@gmail.com>
2019-10-13 13:39:43 +0200

    Copied the React build to the webapp.

---
+++ src/main/webapp/precache-manifest.10b93ccf82a598bf017e702ae98c2b26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8ce70dc371b2f5d3e77df95e3edcd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d220698e82587c12f704",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b227ff3f727bcaa35459",
+    "url": "/static/js/2.cade9933.chunk.js"
+  },
+  {
+    "revision": "d220698e82587c12f704",
+    "url": "/static/js/main.b7a0fead.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
