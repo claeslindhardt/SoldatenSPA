48b54499a9fac5f05c3b4a2097bdf40f74d4bf47
claeslindhardt1 <claeslindhardt@gmail.com>
2019-10-13 13:23:08 +0200

    Made a react app in the web folder. Did not do a npm install yet, nor automate a pipline with the react webapp. The heroku.jar file is working with Rest independently however

--- src/main/java/REST/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package REST;
-
-import javax.ws.rs.ApplicationPath;
-
-//Defines api root
-@ApplicationPath("/rest")
-public class AppConfig {
-
-}
---
+++ src/main/java/rest/AppConfig.java
@@ -0,0 +1,13 @@
+package rest;
+
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+//Defines api root
+@ApplicationPath("/rest")
+public class AppConfig extends Application {
+
+}
