cd05c5b422d700a7fe64b17bac702aaad0e1cdb4
claeslindhardt1 <claeslindhardt@gmail.com>
2019-10-13 16:41:01 +0200

    Played around with some bootstrap.

--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e9f37ce905a77afa0ae32a801f33f1d.js"
+  "/precache-manifest.b0088edb91acd5a632b2938ec05ce1ef.js"
 );
 
 self.addEventListener('message', (event) => {
--- web/src/App.js
+++ web/src/App.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          If you made it this far, it indicates something is working.
-        </p>
+      <header>
+          <Navbar bg="light" expand="lg">
+              <Navbar.Brand href="#home"><img src={logo} cl assName="App-logo" alt="logo" /></Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="mr-auto">
+                      <Nav.Link href="#home">Home</Nav.Link>
+                      <Nav.Link href="#link">Link</Nav.Link>
+                      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                          <NavDropdown.Divider />
+                          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                      </NavDropdown>
+                  </Nav>
+              </Navbar.Collapse>
+          </Navbar>
+
 
       </header>
+      <body className="bodyApp">
+        <p>
+            If you made it this far, it indicates something is working.
+        </p>
+      </body>
     </div>
   );
+
 }
 
 export default App;
