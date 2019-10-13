a90b3ca34df6b09876f2c49119118438eba256b4
claeslindhardt1 <claeslindhardt@gmail.com>
2019-10-13 15:29:53 +0200

    tried making a change to how the plugins are implemented in POM to make the app compatable with Heroku.

---
+++ src/main/webapp/precache-manifest.e039993e277202be381ba810ca781e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0e87367ee1dd6cb2fde51239a6223c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bc776775abce494e6ed",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b227ff3f727bcaa35459",
+    "url": "/static/js/2.cade9933.chunk.js"
+  },
+  {
+    "revision": "9bc776775abce494e6ed",
+    "url": "/static/js/main.4d27e52f.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
