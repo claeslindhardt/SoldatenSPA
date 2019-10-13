1c5c7ce06ccb300250ac37e9969a5711673190a0
claeslindhardt1 <claeslindhardt@gmail.com>
2019-10-13 15:41:57 +0200

    Updated the logo on the page.

---
+++ src/main/webapp/precache-manifest.3e9f37ce905a77afa0ae32a801f33f1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec1f461eef64f03ac51f772774f19cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "360e40d531b87abcf5e1",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b227ff3f727bcaa35459",
+    "url": "/static/js/2.cade9933.chunk.js"
+  },
+  {
+    "revision": "360e40d531b87abcf5e1",
+    "url": "/static/js/main.654def3e.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  },
+  {
+    "revision": "3b4e1b16da8e8f133a63225b3131eb8b",
+    "url": "/static/media/logo.3b4e1b16.png"
+  }
+]);
