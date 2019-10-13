6bdc522c03033753a5cdadf5332306c60039cf24
claeslindhardt1 <claeslindhardt@gmail.com>
2019-10-13 16:41:15 +0200

    Played around with some bootstrap.

---
+++ src/main/webapp/precache-manifest.769ec5a7a625d5fa2906ce563d0b75da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409520ab08313212cd979a38ae0ddf1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f954fbd9c7d12204de45",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "03c45550c26f74d748d2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f954fbd9c7d12204de45",
+    "url": "/static/js/2.7fa24c4d.chunk.js"
+  },
+  {
+    "revision": "03c45550c26f74d748d2",
+    "url": "/static/js/main.927f4003.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  },
+  {
+    "revision": "3b4e1b16da8e8f133a63225b3131eb8b",
+    "url": "/static/media/logo.3b4e1b16.png"
+  }
+]);
---
+++ src/main/webapp/precache-manifest.b0088edb91acd5a632b2938ec05ce1ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0beb2b82ae9c794458a186b3b807a6ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0409bd3ef4afe1d85885",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9344127b9e3083259859",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0409bd3ef4afe1d85885",
+    "url": "/static/js/2.273cfb32.chunk.js"
+  },
+  {
+    "revision": "9344127b9e3083259859",
+    "url": "/static/js/main.cbb109a7.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  },
+  {
+    "revision": "3b4e1b16da8e8f133a63225b3131eb8b",
+    "url": "/static/media/logo.3b4e1b16.png"
+  }
+]);
