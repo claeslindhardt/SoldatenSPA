dfaa574310dba8b7cc6cbc6511a4c4a51a278a15
claeslindhardt1 <claeslindhardt@gmail.com>
2019-10-14 17:17:50 +0200

    Made a build to update Heroku

---
+++ src/main/webapp/precache-manifest.da10996b17d95a5d63d2b49f1af6b95b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c89aeea86e591160f4219eca92e4214",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d612e4c5216e7fbc845f",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ef73e302341565b868e6",
+    "url": "/static/css/main.a4a666fc.chunk.css"
+  },
+  {
+    "revision": "d612e4c5216e7fbc845f",
+    "url": "/static/js/2.38c459fa.chunk.js"
+  },
+  {
+    "revision": "ef73e302341565b868e6",
+    "url": "/static/js/main.cb795ff6.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  },
+  {
+    "revision": "2c262006d4020341cf101a9ba9d7b943",
+    "url": "/static/media/Instalogo.2c262006.png"
+  },
+  {
+    "revision": "3b4e1b16da8e8f133a63225b3131eb8b",
+    "url": "/static/media/logo.3b4e1b16.png"
+  }
+]);
