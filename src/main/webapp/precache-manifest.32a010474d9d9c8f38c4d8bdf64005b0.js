f9b62838f82fe9262ec9ee437a21fd0b9ad28a6a
claeslindhardt1 <claeslindhardt@gmail.com>
2019-10-14 16:38:09 +0200

    Removed the CORS filter, creating a safty issue. to allow Heroku it's ways

---
+++ src/main/webapp/precache-manifest.32a010474d9d9c8f38c4d8bdf64005b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1135ee02978d1c2ff2027db650679a01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d612e4c5216e7fbc845f",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5424dc3255f738021f60",
+    "url": "/static/css/main.a4a666fc.chunk.css"
+  },
+  {
+    "revision": "d612e4c5216e7fbc845f",
+    "url": "/static/js/2.38c459fa.chunk.js"
+  },
+  {
+    "revision": "5424dc3255f738021f60",
+    "url": "/static/js/main.6c4ce6cf.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  },
+  {
+    "revision": "2c262006d4020341cf101a9ba9d7b943",
+    "url": "/static/media/Instalogo.2c262006.png"
+  },
+  {
+    "revision": "3b4e1b16da8e8f133a63225b3131eb8b",
+    "url": "/static/media/logo.3b4e1b16.png"
+  }
+]);
