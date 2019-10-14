8140f2fb483cf812e151202f19eedb1349a94e50
claeslindhardt1 <claeslindhardt@gmail.com>
2019-10-14 17:05:06 +0200

    Tried to change the favicon, without succes, did change the html title of the page however

---
+++ src/main/webapp/precache-manifest.496aa542e5ea79dd9221c7ca05303045.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5384989ac843294b8de36f963de2ffd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d612e4c5216e7fbc845f",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e1a773bab84299ed8ccc",
+    "url": "/static/css/main.a4a666fc.chunk.css"
+  },
+  {
+    "revision": "d612e4c5216e7fbc845f",
+    "url": "/static/js/2.38c459fa.chunk.js"
+  },
+  {
+    "revision": "e1a773bab84299ed8ccc",
+    "url": "/static/js/main.0e9136e0.chunk.js"
+  },
+  {
+    "revision": "131f303e7d4726167875",
+    "url": "/static/js/runtime-main.74b9062d.js"
+  },
+  {
+    "revision": "2c262006d4020341cf101a9ba9d7b943",
+    "url": "/static/media/Instalogo.2c262006.png"
+  },
+  {
+    "revision": "3b4e1b16da8e8f133a63225b3131eb8b",
+    "url": "/static/media/logo.3b4e1b16.png"
+  }
+]);
