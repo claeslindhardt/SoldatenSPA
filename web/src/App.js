6f0df41775d111f64cb120727a93dd3d9b6c9eb0
claeslindhardt1 <claeslindhardt@gmail.com>
2019-10-14 13:06:37 +0200

    Added routing refrences to external webpages and updated the Nav bar

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import logo from './logo1.png';
+import logo from './Logos/logo1.png';
+import FB from './Logos/FBlogo.png';
+import Insta from './Logos/Instalogo.png';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Route from "react-router-dom/Route";
+import Switch from "react-bootstrap/Switch";
 
 
 function App() {
@@ -13,17 +18,20 @@ function App() {
       <header>
           <Navbar bg="light" expand="lg">
               <Navbar.Brand href="#home"><img src={logo} alt="Slogo" width={50} height={50} /></Navbar.Brand>
+              <Navbar.Brand href="#home"><img src={FB} alt="Slogo" width={50} height={50} /></Navbar.Brand>
+              <Navbar.Brand href="#home"><img src={Insta} alt="Slogo" width={30} height={30} /></Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="mr-auto">
-                      <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="#link">Link</Nav.Link>
-                      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                      <Nav.Link href="#/Home">Startside</Nav.Link>
+                      <Nav.Link href="#Fifkassen">Fifkassen</Nav.Link>
+                      <Nav.Link href="https://issuu.com/soldaten">Arkiv</Nav.Link>
+                      <NavDropdown title="Readaktionen" id="basic-nav-dropdown">
+                          <NavDropdown.Item href="#action/3.1">Kontakt</NavDropdown.Item>
+                          <NavDropdown.Item href="#action/3.2">Job hos soldaten</NavDropdown.Item>
+                          <NavDropdown.Item href="#action/3.3">Brevkasse</NavDropdown.Item>
                           <NavDropdown.Divider />
-                          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                          <NavDropdown.Item href="#action/3.4">Ønsker til redaktionen.</NavDropdown.Item>
                       </NavDropdown>
                   </Nav>
               </Navbar.Collapse>
@@ -35,11 +43,24 @@ function App() {
         <p>
             If you made it this far, it indicates something is working.
         </p>
-        <img src={logo} cl assName="App-logo" alt="logo" rounded/>
+        <Switch>
+            <Route path={"/home"} component={Home}/>
+            <Route exact path={"/"} render={()=><h1>Startside</h1>}/>
+        </Switch>
       </body>
     </div>
   );
 
 }
 
+
+
 export default App;
+
+
+const Home = ()=>{
+    return <div>
+        <h1>Startside</h1>
+        <img src={logo} cl assName="App-logo" alt="logo" rounded/>
+    </div>
+};
